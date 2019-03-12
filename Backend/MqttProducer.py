a4ac5f15a93914b7b461a404c0c583351fdf4092
Sunnex <sunnexajayi@gmail.com>
2019-03-12 11:39:13 +0000

    New update on the mqtt file

--- Backend/MqttProducer.py
+++ Backend/MqttProducer.py
@@ -2,16 +2,17 @@
 The Mqtt Producer get the name of the last file uploaded
 to the Google Cloud Storage.
 '''
-# Import needed packages
 
-import os,sys
+# Import needed packages
+import os
+import sys
 
 # Write a shell command and saves it in the output variable
-imageNames= []
-videoNames= []
+imageNames = []
+videoNames = []
 
-fileImages=os.popen("gsutil ls gs://media_2019/images")
-fileVideos=os.popen("gsutil ls gs://media_2019/videos")
+fileImages = os.popen("gsutil ls -lh gs://media_2019/images | sort -k 2")
+fileVideos = os.popen("gsutil ls -lh  gs://media_2019/videos | sort -k 2")
 
 print("Image Names: ")
 for imageName in fileImages.readlines():
@@ -20,10 +21,11 @@ for imageName in fileImages.readlines():
 
 print("****************")
 
-print("Video Names: ")
+# print("Video Names: ")
 for videoName in fileVideos.readlines():
     videoNames.append(videoName[23:len(videoName)-1])
     print(videoName[23:len(videoName)-1])
 
-
-
+print("Latest Upload: ")
+print(imageNames[-1])
+print(videoNames[-1])
--- Backend/flasktest.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/hello')
-def hello_name():
-   return 'Welcome to Flask world'
-
-if __name__ == '__main__':
-    app.run(debug=True) 
